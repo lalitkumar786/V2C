6a09e89df835f043a83d71beff1fd4978880547a
Lalit Mishra <lalit.mishra@gaussnetworks.com>
2019-01-29 16:34:53 +0530

    first commit

---
+++ V2CSolutions/V2CSolutions/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace V2CSolutions
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
